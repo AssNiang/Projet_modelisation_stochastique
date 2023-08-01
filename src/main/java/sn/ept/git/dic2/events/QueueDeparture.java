462a7ff1b36b1bc907d3d37e89a09577cdac146c
Ass NIANG <nianga@ept.sn>
2023-08-01 01:09:10 +0000

    update: QueueDeparture Event

--- src/main/java/sn/ept/git/dic2/events/QueueDeparture.java
+++ src/main/java/sn/ept/git/dic2/events/QueueDeparture.java
@@ -19,24 +19,26 @@ public class QueueDeparture extends Event {
     public void actions() {
         int type = rd.getMap().get(cust.getType());
         int queue_length = rd.getArray_queue_length()[type];
-//      Mettre a jours les donnees utilisees par les predicteurs
-        rd.updateArray_LES(type, cust.getWaiting_time());
 
-        rd.updateArray_Avg_LES(type, cust.getWaiting_time());
-        rd.updateArray_AvgC_LES(type, queue_length, cust.getWaiting_time());
-
-
-        rd.updateArray_WAvgC_LES(type, queue_length, cust.getWaiting_time(), 0.5);
-//         Decrementer le nb cust de ce type()  (array_queue_length)
+        // Decrementer le nb cust de ce type()  (array_queue_length)
         rd.updateArray_queue_length(type, -1);
-//         Si depart == abandon alors mettre cust dans la liste des cust abandonnes  (abandon_customer)
+        // Si depart == abandon alors
         if (!cust.getIs_served()) {
+            // mettre cust dans la liste des cust abandonnes  (abandon_customer)
             rd.addAbandon_customer(cust);
-        } else {
+        }
+        // Si depart == debutService alors
+        else {
+            // Mettre a jours les donnees utilisees par les predicteurs
+            rd.updateArray_LES(type, cust.getWaiting_time());
+            rd.updateArray_Avg_LES(type, cust.getWaiting_time());
+            rd.updateArray_AvgC_LES(type, queue_length, cust.getWaiting_time());
+            rd.updateArray_WAvgC_LES(type, queue_length, cust.getWaiting_time(), 0.2);
+            // mettre cust dans la liste des cust servis
             rd.addServed_customer(cust);
-            //         Si depart == debutService alors incrementer nb_busy_servers
+            // incrementer nb_busy_servers
             rd.setNb_busy_servers(rd.getNb_busy_servers() + 1);
-            //              schudeler la fin de service
+            // schudeler la fin de service
             new EndOfService(rd).schedule(cust.getService_time());
         }
 
